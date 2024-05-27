08335d8780222f8e6275515b843577d41360b24d
AnshulNewron <anshul@newron.ai>
2024-05-27 17:33:45 +0530

    fixed the state not displaying issue

--- components/Choropleth.js
+++ components/Choropleth.js
@@ -1,54 +1,79 @@
-import React, {memo, useEffect, useMemo} from 'react';
+import React, { memo, useEffect, useMemo } from 'react';
 
-import {ResponsiveChoropleth} from '@nivo/geo';
+import { ResponsiveChoropleth } from '@nivo/geo';
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
 import PropTypes from "prop-types";
-import {MAP_AREAS} from "../src/choroplethHelpers";
+import { MAP_AREAS } from "../src/choroplethHelpers";
 import useCurrentTheme from "../common/hooks/useCurrentTheme";
+
+const correctDistrictName = {
+    "Anantapur": "Anantpur",
+    "Visakhapatnam": "Vishakapattanam",
+    "East Champaran": "EastChamparan",
+    "Jehanabad": "Jahanabad",
+    "Kabeerdham": "Kabirdham",
+    "Surguja": "Sarguja",
+    "Sahibganj": "Sahebganj",
+    "Belagavi": "Belgaum",
+    "Ballari": "Bellary",
+    "Chamarajanagara": "Chamrajnagar",
+    "Dakshina Kannada": "DakshinKannada",
+    "Kalaburagi": "Gulbarga",
+    "Mysuru": "Mysore",
+    "Shivamogga": "Shimoga",
+    "Vijayapura": "Bijapur",
+    "Sindhudurg": "Sindhudurga",
+    "Tehri Garhwal": "TehriGarhwal",
+    "Dakshin Dinajpur": "DakshinDinajpur",
+    "Kolkata": "Kolkata",
+    "North 24 Parganas": "North24Parganas",
+    "Paschim Medinipur": "PaschimMedinipur",
+    "Amroha": "JyotibaPhuleNagar",
+    "Muzaffarnagar": "Muzzaffarnagar"
+}
 
 const jsonPath = (region) => `/maps/${region}.json`;
 const Choropleth = memo(({
-                            region = "india",
-                            dataLevel = "districts",
-                            data = [],
-                            domain = [0, 50000],
-                            unknownColor,
-                            valueFormat = ".2s",
-                            colors = "nivo",
-                            graticuleColor = "#f68c8c",
-                            enableLegend = true,
-                            noStateName = false,
-                            projectionTranslation= [0.5, 1.12],
-                            projectionRotation=[-83, 0, 0],
-                            projectionScale=800,
-                            legend = [
-                                {
-                                    anchor: 'bottom',
-                                    direction: 'column',
-                                    justify: true,
-                                    translateX: -183,
-                                    translateY: -45,
-                                    itemsSpacing: 0,
-                                    itemWidth: 82,
-                                    itemHeight: 24,
-                                    itemDirection: 'left-to-right',
-                                    itemTextColor: '#444444',
-                                    itemOpacity: 0.85,
-                                    symbolSize: 21,
-                                    effects: [
-                                        {
-                                            on: 'hover',
-                                            style: {
-                                                itemTextColor: '#000000',
-                                                itemOpacity: 1
-                                            }
-                                        }
-                                    ]
-                                }
-                            ], ...rest
-                         },) =>
-{
+    region = "india",
+    dataLevel = "districts",
+    data = [],
+    domain = [0, 50000],
+    unknownColor,
+    valueFormat = ".2s",
+    colors = "nivo",
+    graticuleColor = "#f68c8c",
+    enableLegend = true,
+    noStateName = false,
+    projectionTranslation = [0.5, 1.12],
+    projectionRotation = [-83, 0, 0],
+    projectionScale = 800,
+    legend = [
+        {
+            anchor: 'bottom',
+            direction: 'column',
+            justify: true,
+            translateX: -183,
+            translateY: -45,
+            itemsSpacing: 0,
+            itemWidth: 82,
+            itemHeight: 24,
+            itemDirection: 'left-to-right',
+            itemTextColor: '#444444',
+            itemOpacity: 0.85,
+            symbolSize: 21,
+            effects: [
+                {
+                    on: 'hover',
+                    style: {
+                        itemTextColor: '#000000',
+                        itemOpacity: 1
+                    }
+                }
+            ]
+        }
+    ], ...rest
+},) => {
 
     const [mapLoadingError, setMapLoadingError] = React.useState(null);
     const [features, setFeatures] = React.useState(null);
@@ -75,18 +100,26 @@ const Choropleth = memo(({
 
 
     useEffect(() => {
-        if(!region) {
+        if (!region) {
             return;
         }
         d3.json(jsonPath(region)).then((topology) => {
             const features = topojson.feature(topology, topology.objects?.[dataLevel]).features;
-
-            if(dataLevel === "districts") {
+            if (dataLevel === "districts") {
                 features.forEach((feature) => {
-                    feature.id = noStateName ? feature.properties.district :`${feature.properties.st_nm}-${feature.properties.district}`;
+                    let district = feature.properties.district;
+                    if (correctDistrictName[district]) {
+                        district = correctDistrictName[district];
+                    }
+                    feature.id = noStateName ? district : `${feature.properties.st_nm}-${district}`;
                 })
             }
-
+            if(dataLevel === "states") {
+                features.forEach((feature) => {
+                    let state = feature.properties.st_nm.replace(/\s+/g, '');
+                    feature.id = state;
+                })
+            }
             setFeatures(features);
         }).catch((error) => {
             setMapLoadingError(error);
@@ -94,7 +127,7 @@ const Choropleth = memo(({
     }, [region, dataLevel]);
 
 
-    if(!features) {
+    if (!features) {
         return null;
     }
 
@@ -114,7 +147,7 @@ const Choropleth = memo(({
                 graticuleLineColor={graticuleColor}
                 margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
                 colors={colors}
-                legends={ processedLegends}
+                legends={processedLegends}
                 {...rest}
             />
         </div>
